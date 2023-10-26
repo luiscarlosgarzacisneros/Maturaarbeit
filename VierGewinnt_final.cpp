e3d4a46574906f4dcb4bbacbf29fbab3eb2fe703
luiscarlosgarzacisneros <111580548+luiscarlosgarzacisneros@users.noreply.github.com>
2023-10-26 23:58:08 +0200

    Update VierGewinnt_final.cpp output

--- VierGewinnt_final.cpp
+++ VierGewinnt_final.cpp
@@ -756,12 +756,12 @@ private:
 //
 
 void spielen(int z) {
-    std::cout<<"NEUES SPIEL"<<std::endl;
     int x_wins=0;
     int o_wins=0;
     int unentschieden=0;
     //
     for (int i=0; i<z; ++i) {
+        std::cout<<"NEUES SPIEL"<<std::endl;
         VierGewinnt game;
         int r = game.play();
         if (r==1) {x_wins+=1;}
